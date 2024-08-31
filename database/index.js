bdf79efdbe196b6abe182e6e261852c97739876c
Muaz <muazzia17@gmail.com>
2024-08-31 13:35:14 +0500

    change connection string

--- database/index.js
+++ database/index.js
@@ -3,11 +3,16 @@ const { Sequelize } = require("sequelize")
 
 
 // const sequelize = new Sequelize('postgres://postgres:pass@example.com:5432/dbname')
-const sequelize = new Sequelize('fyp', 'postgres', 'Muazshah1235#', {
-    host: 'localhost',
-    dialect: "postgres",
+
+const sequelize = new Sequelize(db, {
     logging: false
-    // dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-});
+})
+
+// const sequelize = new Sequelize('fyp', 'postgres', 'Muazshah1235#', {
+//     host: 'localhost',
+//     dialect: "postgres",
+//     logging: false
+//     // dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+// });
 
 module.exports = sequelize
