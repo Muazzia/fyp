69c7ad775f555f9e0ba85e5c1ccc020f1c262431
Muaz <muazzia17@gmail.com>
2024-09-14 19:44:42 +0500

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 require("express-async-errors")
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const sequelize = require("./database");
 require("./association")
 const router = require("./routes");
 const app = express();
 
+
+
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
+}))
+
+app.use(cors({
+    origin: "*"
 }))
 
 app.use("/v1/api", router)
