a1895f37e2c01e759ef2e027a0c1e24425da1ea1
Muaz <muazzia17@gmail.com>
2024-10-29 22:33:33 +0500

    created get all produtt admin route

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createAProduct, deleteAProduct, updateAProduct } = require("../../controller/admin/product");
+const { createAProduct, deleteAProduct, updateAProduct, getAllproducts } = require("../../controller/admin/product");
 const { uploadImages } = require("../../middleware/multer");
 const productRouter = express.Router();
 
@@ -7,6 +7,7 @@ const productRouter = express.Router();
 productRouter.post("/", uploadImages({ fieldName: "image", isSinlge: true }), createAProduct);
 productRouter.put("/:id", uploadImages({ fieldName: "image", isRequired: false, isSinlge: true }), updateAProduct);
 productRouter.delete("/:id", deleteAProduct)
+productRouter.get("/", getAllproducts)
 
 
 
